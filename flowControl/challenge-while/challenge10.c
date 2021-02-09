b35eb3bfbfbe00881aae8892e5067e06ab28f3a3
Christian MV <chris.mlvz@gmail.com>
2021-02-09 13:50:03 -0600

    challenge10: while loop

---
+++ flowControl/challenge-while/challenge10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Imprime 5# mediante while\n");
+    int limit = 5;
+    int i = 1;
+    while(i <= limit)
+    {
+        printf("#\n");
+        i++;
+    }
+    return 0;
+}
