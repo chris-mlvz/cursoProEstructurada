95d211d73a68c6cb9c2f134037f7b42cd7581543
Christian MV <chris.mlvz@gmail.com>
2021-02-10 21:04:33 -0600

    Create new File

---
+++ advancedConcepts/Sruct-and-fileManagement/createFile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Archivos! Crear un archivo\n");
+    FILE *archivo;
+    archivo = fopen("prueba3.dat", "w");
+    if (archivo != NULL)
+    {
+        printf("El archivo se ha creado exitosamente. Comprueba en la carpeta que ha sido creado \n");
+        fclose(archivo);
+    }
+    else
+    {
+        printf("El archivo no se ha creado");
+    }
+    return 0;
+}
